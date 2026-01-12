12f7d5792bc3b59f790d0a534901f7b2f34edbc4
Lady R'n'B <rahma.barrak.k@gmail.com>
2026-01-12 12:06:56 +0100

    first setup

---
+++ src/main/java/com/kuchelib/erecipes/ErecipesApplication.java
@@ -0,0 +1,13 @@
+package com.kuchelib.erecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErecipesApplication.class, args);
+	}
+
+}
