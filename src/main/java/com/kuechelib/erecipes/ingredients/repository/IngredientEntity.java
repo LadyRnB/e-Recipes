e8c361e6f951667c1184f3722d5ff112f5a24073
Lady R'n'B <rahma.barrak.k@gmail.com>
2026-01-18 17:11:22 +0100

    GeneratedValue strategy set to identity

--- src/main/java/com/kuechelib/erecipes/ingredients/repository/IngredientEntity.java
+++ src/main/java/com/kuechelib/erecipes/ingredients/repository/IngredientEntity.java
@@ -1,10 +1,7 @@
 package com.kuechelib.erecipes.ingredients.repository;
 
 import com.kuechelib.erecipes.ingredients.controller.MeasurementUnit;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class IngredientEntity {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String attribute;
